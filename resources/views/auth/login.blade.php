43297691237133b7038124c9ce0dc55ea303e55f
Erik C. Olson <erikcaineolson@gmail.com>
2016-02-15 20:28:58 -0800

    Check for existence of $errors in login form before using the variable

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,13 +10,13 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group{{ isset($errors) ? $errors->has('email') ? ' has-error' : '' : '' }}">
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
+                                @if (isset($errors) and $errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
@@ -24,13 +24,13 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group{{ isset($errors) ? $errors->has('password') ? ' has-error' : '' : '' }}">
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
 
-                                @if ($errors->has('password'))
+                                @if (isset($errors) && $errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
