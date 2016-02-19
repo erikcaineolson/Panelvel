aed526071e2d8aba8bbbbe38b8e148c89bd58179
Erik C. Olson <erikcaineolson@gmail.com>
2016-02-18 19:08:28 -0800

    Finish perl installation script

--- storage/perl/install.pl
+++ storage/perl/install.pl
@@ -3,19 +3,35 @@
 use strict;
 use warnings;
 use DBI;
+use String::Random;
 
 my $in_file;
 my $web_dir;
 my @lines;
 my $line;
+my @nginx_lines;
+my $nginx_line;
+my @php_lines;
+my $php_line;
+my @wp_lines;
+my $wp_line;
 
 my $domain;
 my $user;
 my $pass;
 my $is_wp;
-my $wp_db;
-my $wp_db_user;
-my $wp_db_pass;
+
+my $random_pattern;
+my $random_string;
+
+my $wp_db = '';
+my $wp_db_user = '';
+my $wp_db_pass = '';
+
+my $wp_source = 'https://wordpress.org/latest.tar.gz';
+
+my $nginx_config_file;
+my $php_config_file;
 
 if (@ARGV && $ARGV[0] ne '' && $ARGV[1] ne '') {
     $in_file = $ARGV[0];
@@ -29,10 +45,36 @@ if (@ARGV && $ARGV[0] ne '' && $ARGV[1] ne '') {
     foreach $line (@lines)
     {
         # break up the line into components
-        ($domain, $user, $pass, $is_wp, $wp_db, $wp_db_user, $wp_db_pass) = split(':', $line);
+        ($domain, $user, $pass, $is_wp, $wp_db, $wp_db_user, $wp_db_pass, $nonce_0, $nonce_1, $nonce_2, $nonce_3, $nonce_4, $nonce_5, $nonce_6, $nonce_7) = split(':', $line);
+        @nonces = [$nonce_0, $nonce_1, $nonce_2, $nonce_3, $nonce_4, $nonce_5, $nonce_6, $nonce_7];
 
         # create the user
-        system('useradd'  . $user . ' -h ' . $web_dir . '-G www-data -p ' . $pass);
+        system('useradd'  . $user . ' -h ' . $web_dir . '/' . $user . ' -G www-data -p ' . $pass);
+
+        # create the user's SFTP directory
+        system('mkdir -p /srv/sftp/' . $user . '/web');
+
+        # mount the user's home directory to the SFTP/user/web directory
+        system('mount --bind /srv/sftp/' . $user . '/web ' . $web_dir);
+
+        # edit the config files
+        open(PHP_CONFIG, '+<', '../sites/php/' . $domain . '.conf');
+        @php_lines = <PHP_CONFIG>;
+        foreach $php_line (@php_lines)
+        {
+            $php_line =~ s/SITE_NAME/\$domain/g;
+        }
+        print PHP_CONFIG @php_lines;
+        close(PHP_CONFIG);
+
+        open(NGINX_CONFIG, '+<', '../sites/nginx/' . $domain);
+        @nginx_lines = <NGINX_CONFIG>;
+        foreach $nginx_line (@nginx_lines)
+        {
+            $nginx_line =~ s/SITE_NAME/\$domain/g;
+        }
+        print NGINX_CONFIG @nginx_lines;
+        close(NGINX_CONFIG);
 
         # move the config files
         system('mv ../sites/nginx/' . $domain . ' /etc/nginx/sites-available/');
@@ -46,6 +88,34 @@ if (@ARGV && $ARGV[0] ne '' && $ARGV[1] ne '') {
         system('mkdir', '-p', $web_dir . '/' . $domain . '/logs');
         system('touch', $web_dir . '/' . $domain . '/logs/access.log');
         system('touch', $web_dir . '/' . $domain . '/logs/error.log');
+
+        # if it's wordpress, install wordpress
+        if($is_wp == 1){
+            system('wget ' . $wp_source . ' -O ' . $web_dir . '/' . $user . '/public_html/latest.tar.gz');
+            system('tar -xf ' . $web_dir . '/' . $user . '/public_html/latest.tar.gz');
+            system('mv ' . $web_dir . '/' . $user . '/public_html/latest.tar.gz/* ../');
+            system('rmdir ' . $web_dir . '/' . $user . '/public_html/wordpress');
+
+            open(WP_CONFIG_SAMPLE, '<', $web_dir . '/' . $user . '/public_html/wordpress/wp-config-sample.php');
+            open(WP_CONFIG, '>', $web_dir . '/' . $user . '/public_html/wordpress/wp-config.php');
+            @wp_lines = <WP_CONFIG_SAMPLE>;
+
+            foreach $wp_line (@wp_lines)
+            {
+                $random_pattern = new String::Random();
+                $random_string = $random_pattern->randpattern("................................................................");
+
+                $wp_line =~ s/database_name_here/\$wp_db/;
+                $wp_line =~ s/username_here/\$wp_db_user/;
+                $wp_line =~ s/password_here/\$wp_db_pass/;
+                $wp_line =~ s/put your unique phrase here/\$random_string/;
+
+                print WP_CONFIG $wp_line;
+            }
+
+            close(WP_CONFIG);
+            close(WP_CONFIG_SAMPLE);
+        }
 
         # restart NGINX and PHP
         system('service nginx restart');
