87f6610b7cd4d333d1b8a6f8213859afe67a84c5
Erik C. Olson <erikcaineolson@gmail.com>
2016-02-19 23:52:13 -0800

    Remove registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,8 @@ Route::group(['middleware' => ['web']], function () {
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
+    Route::any('/auth/register', 'HomeController@index');
+
     Route::get('/home', 'HomeController@index');
 
     // Domain Routes
