9a8335973de58dafaa24b7f88aea70fa5f7c0adf
Erik C. Olson <erikcaineolson@gmail.com>
2018-09-03 12:23:27 -0700

    Add Create User Command
    Enable server admin to create users from the Artisan command console.

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,81 @@
+<?php namespace App\Console\Commands;
+
+use App\User;
+use Exception;
+use Illuminate\Console\Command;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $emailTries = 0;
+        $passwordTries = 0;
+
+        // get the email, provided we don't already have a user in the system with this email
+        do {
+            if ($emailTries > 0) {
+                $this->error('Email exists, please try again.');
+            }
+
+            $email = $this->ask('Please enter the email: ');
+            $emailTries++;
+        } while (User::where('email', $email)->count() > 0);
+
+        // get the password, provided the passwords match
+        do {
+            if ($passwordTries > 0) {
+                $this->error('Password exists, please try again.');
+            }
+
+            $password = $this->secret('Please enter the user\'s password: ');
+            $confirm = $this->secret('Please confirm the user\'s password: ');
+            $passwordTries++;
+        } while ($password != $confirm);
+
+        $firstName = $this->ask('Please enter the user\'s first name: ');
+        $lastName = $this->ask('Plesae enter the user\'s last name: ');
+
+        try {
+            User::create([
+                'first_name' => $firstName,
+                'last_name'  => $lastName,
+                'email'      => $email,
+                'password'   => bcrypt($password),
+            ]);
+        } catch (Exception $e) {
+            $this->error($e->getMessage());
+            exit(1);
+        }
+
+        $this->info('User ' . $email . ' has been successfully added!');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CreateUser;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,8 +14,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
         Commands\InstallSite::class,
+        CreateUser::class,
     ];
 
     /**
